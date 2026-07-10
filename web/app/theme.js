868a45bb474e11aad8594b2bc2cf8d4e1e4531c8
kamilmac <kmacinski@gmail.com>
2026-07-10 16:55:48 +0200

    18 curated palettes in the theme modal

--- web/app/theme.js
+++ web/app/theme.js
@@ -19,6 +19,18 @@ const THEMES = [
   ['forest', { surface: '#1d2a23', kick: '#7fb069', snare: '#e6aa68', hihat: '#cfe1b9', master: '#ecf4e7' }],
   ['dusk', { surface: '#241f3d', kick: '#8e7dff', snare: '#ff7edb', hihat: '#ffd166', master: '#efeafc' }],
   ['port', { surface: '#2a1a24', kick: '#d4707e', snare: '#e8b04b', hihat: '#9fd8cb', master: '#f2e9e4' }],
+  ['ocean', { surface: '#132430', kick: '#2e86ab', snare: '#f6ae2d', hihat: '#a7d3e0', master: '#eaf4f4' }],
+  ['ember', { surface: '#241518', kick: '#f25c54', snare: '#f4a261', hihat: '#ffe1c6', master: '#fff4ec' }],
+  ['moss', { surface: '#212a1e', kick: '#87a878', snare: '#d9bf77', hihat: '#eff7cf', master: '#f4f7ee' }],
+  ['arctic', { surface: '#1b2432', kick: '#5390d9', snare: '#ffd6ba', hihat: '#cae9ff', master: '#f1f6f9' }],
+  ['sakura', { surface: '#2b2028', kick: '#d88bab', snare: '#9ad1aa', hihat: '#f6e7cb', master: '#fbf3f5' }],
+  ['taxi', { surface: '#1b1b1b', kick: '#ffd60a', snare: '#ff453a', hihat: '#e8e8e8', master: '#fffbe6' }],
+  ['mint', { surface: '#17251f', kick: '#2ec4b6', snare: '#ff9f1c', hihat: '#cbf3f0', master: '#f0fbf9' }],
+  ['grape', { surface: '#251b30', kick: '#9d4edd', snare: '#ffc857', hihat: '#d0bdf4', master: '#f3edfa' }],
+  ['copper', { surface: '#211a14', kick: '#bf7145', snare: '#ddb967', hihat: '#d8e2dc', master: '#f2ebe3' }],
+  ['lagoon', { surface: '#113537', kick: '#26a69a', snare: '#e4c988', hihat: '#b2dfdb', master: '#ecf7f6' }],
+  ['cobalt', { surface: '#14213d', kick: '#fca311', snare: '#e5e5e5', hihat: '#98c1d9', master: '#ffffff' }],
+  ['rose noir', { surface: '#1e151c', kick: '#ef476f', snare: '#ffd166', hihat: '#06d6a0', master: '#f7f0f3' }],
 ];
 
 let onChange = () => {};
